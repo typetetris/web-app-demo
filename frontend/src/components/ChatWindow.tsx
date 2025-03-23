21b86066975baf1beb932a3115c1d31878f74e7a
Eric Wolf <1983821+typetetris@users.noreply.github.com>
2025-03-23 16:07:40 +0100

    feat(web-app-demo-frontend): Add ChatWindow component

--- frontend/src/components/ChatWindow.tsx
+++ frontend/src/components/ChatWindow.tsx
@@ -1,19 +1,50 @@
-import { Content, Flex } from "@adobe/react-spectrum";
-import { Chat } from "../models/Chat";
+import { Flex, Heading, View } from "@adobe/react-spectrum";
 import { Identity } from "../models/Identity";
+import { ChatMessage } from "../util/chatClient";
+import { DOMRef, StyleProps } from "@react-types/shared";
+import { Chat } from "../models/Chat";
+import { ChatMessagesView } from "./ChatMessagesView";
+import { SendChatMessageForm } from "./SendChatMessageForm";
+import { useRef } from "react";
 
-export interface ChatWindowProps {
-  chat: Chat | null;
-  identity: Identity | null;
+export interface ChatWindowProps extends StyleProps {
+  messages: ChatMessage[];
+  identity: Identity;
+  chat: Chat;
+  onSend: (msg: string) => void;
 }
-
-export function ChatWindow({ chat, identity }: ChatWindowProps) {
+export function ChatWindow({
+  messages,
+  identity,
+  chat,
+  onSend,
+  ...styleProps
+}: ChatWindowProps) {
+  const chatMessagesViewRef: DOMRef = useRef(null);
   return (
-    <Flex direction="column">
-      <Content>Chat: {chat?.name ?? "no chat selected"}</Content>
-      <Content>
-        Identity: {identity?.displayName ?? "no identity selectod"}
-      </Content>
+    <Flex direction="column" justifyContent="stretch" {...styleProps}>
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="baseline"
+        flex="0 0 auto"
+      >
+        <Heading level={2}>{chat.name}</Heading>
+        <View>Sie chatten als: {identity.displayName}</View>
+      </Flex>
+      <ChatMessagesView
+        messages={messages}
+        identity={identity}
+        chat={chat}
+        flex="1 1 auto"
+        ref={chatMessagesViewRef}
+      />
+      <SendChatMessageForm
+        onSubmit={onSend}
+        onScrollToBottom={() => {}}
+        flex="0 0 auto"
+        marginX="size-300"
+      />
     </Flex>
   );
 }
